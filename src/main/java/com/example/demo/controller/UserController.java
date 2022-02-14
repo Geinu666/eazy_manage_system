1408174ca7df285506c07c9cbad9796bf0763db7
Geinu666 <51981331+Geinu666@users.noreply.github.com>
2022-02-14 12:46:34 +0800

    First commmit

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.mapper.UserMapper;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public Integer save(@RequestBody User user){
+        return userService.save(user);
+    }
+    @GetMapping
+    public List<User> index(){
+        return userMapper.findAll();
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,72 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String nickname;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    private String email;
+    private String phone;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
