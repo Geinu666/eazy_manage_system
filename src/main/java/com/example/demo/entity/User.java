8ac0758590298e368a4d819984a24a5e3a2e7ee0
Geinu666 <51981331+Geinu666@users.noreply.github.com>
2022-02-19 21:12:39 +0800

    Changed into code generator ver.

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,88 +1,57 @@
 package com.example.demo.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
-@TableName(value = "sys_user")
-public class User {
-    @TableId(type = IdType.AUTO)
-    private Integer id;
-    private String username;
-    @JsonIgnore//忽略字段
-    private String password;
-    private String nickname;
-    private String email;
-    private String phone;
-    private String address;
-    @TableField(value = "avatar_url")
-    private String avatar;
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author Geinu
+ * @since 2022-02-19
+ */
+@Getter
+@Setter
+  @TableName("sys_user")
+@ApiModel(value = "User对象", description = "")
+public class User implements Serializable {
 
-    public String getAvatar() {
-        return avatar;
-    }
+    private static final long serialVersionUID = 1L;
 
-    public void setAvatar(String avatar) {
-        this.avatar = avatar;
-    }
+      @ApiModelProperty("ID")
+        @TableId(value = "id", type = IdType.AUTO)
+      private Integer id;
 
-    public String getNickname() {
-        return nickname;
-    }
+      @ApiModelProperty("用户名")
+      private String username;
 
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
+      @ApiModelProperty("密码")
+      private String password;
 
-    public String getPhone() {
-        return phone;
-    }
+      @ApiModelProperty("昵称")
+      private String nickname;
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+      @ApiModelProperty("邮箱")
+      private String email;
 
-    public Integer getId() {
-        return id;
-    }
+      @ApiModelProperty("电话")
+      private String phone;
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+      @ApiModelProperty("地址")
+      private String address;
 
-    public String getUsername() {
-        return username;
-    }
+      @ApiModelProperty("创建时间")
+      private LocalDateTime createTime;
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    private String avatarUrl;
 
-    public String getPassword() {
-        return password;
-    }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
--- src/main/java/com/example/demo/utils/CodeGenerator.java
+++ src/main/java/com/example/demo/utils/CodeGenerator.java
@@ -11,19 +11,22 @@ public class CodeGenerator {
         generate();
     }
     private static void generate() {
-        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test?serverTimezone=GMT%2b8", "root", "bobo84423737")
+        FastAutoGenerator.create("jdbc:mysql://localhost:3306/test?serverTimezone=GMT%2b8", "root", "12341234")
                 .globalConfig(builder -> {
                     builder.author("Geinu") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir("F:\\springboot-learn\\src\\main\\java"); // 指定输出目录
+                            .outputDir("C:\\Users\\45212\\IdeaProjects\\eazy_manage_system\\src\\main\\java\\"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
                     builder.parent("com.example.demo") // 设置父包名
-                            .moduleName("") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "F:\\springboot-learn\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
+                            .moduleName(null) // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "C:\\Users\\45212\\IdeaProjects\\eazy_manage_system\\src\\main\\resources\\mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
+                    builder.entityBuilder().enableLombok();
+                    builder.mapperBuilder().enableMapperAnnotation().build();
+                    builder.controllerBuilder().enableHyphenStyle().enableRestStyle();
                     builder.addInclude("sys_user") // 设置需要生成的表名
                             .addTablePrefix("t_", "sys_"); // 设置过滤表前缀
                 })
