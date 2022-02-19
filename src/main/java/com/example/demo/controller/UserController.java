b8627580152f789ceb224d5f81b23a1aad53fb64
Geinu666 <51981331+Geinu666@users.noreply.github.com>
2022-02-19 21:13:27 +0800

    Changed into code generator ver.

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,75 @@
+package com.example.demo.controller;
+
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import org.springframework.web.bind.annotation.*;
+import javax.annotation.Resource;
+import java.util.List;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import com.example.demo.service.IUserService;
+import com.example.demo.entity.User;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author Geinu
+ * @since 2022-02-19
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private IUserService userService;
+
+    @PostMapping
+    public boolean save(@RequestBody User user) {
+        return userService.saveOrUpdate(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public Boolean delete(@PathVariable Integer id) {
+        return userService.removeById(id);
+    }
+
+    @GetMapping
+    public List<User> findAll(){
+        return userService.list();
+    }
+
+    @GetMapping("/{id}")
+    public User findOne(@PathVariable Integer id){
+        return userService.getById(id);
+    }
+
+    @GetMapping("/page")
+    public Page<User> findPage(@RequestParam Integer pageNum,
+                               @RequestParam Integer pageSize,
+                               @RequestParam(defaultValue = "") String username,
+                               @RequestParam(defaultValue = "") String email,
+                               @RequestParam(defaultValue = "") String address){
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("id");
+        if (!"".equals(username)){
+            queryWrapper.like("username", username);
+        }
+        if (!"".equals(email)){
+            queryWrapper.like("email", email);
+        }
+        if (!"".equals(address)){
+            queryWrapper.like("address", address);
+        }
+        return userService.page(new Page<>(pageNum, pageSize), queryWrapper);
+    }
+
+    @PostMapping("/del/batch")
+    public boolean deleteBatch(@RequestBody List<Integer> ids) {
+        return userService.removeByIds(ids);
+    }
+}
+
